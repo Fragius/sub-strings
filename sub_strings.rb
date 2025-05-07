047899d38a2f1e835f235a5b8e0dfe16c2ccf22a
fragius <189782341+Fragius@users.noreply.github.com>
2025-05-07 11:49:04 +0300

    Write #sub_strings
    Write #substrings to count the amount of times
    a each word in the dictionary appears.

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,3 +1,15 @@
 def count_word (word, string)
- string.downcase.scan(word.downcase).length
+ string.downcase.scan(word).length
+end
+
+def substrings (string, dictionary)
+  total_count = Hash.new()
+  dictionary.each do |word|
+    count = count_word word, string
+    if count > 0
+      key = word.to_sym
+      total_count[key] = count
+    end
+  end
+  total_count
 end
